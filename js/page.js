ce532e75188122adaa96ee903ebc74017ef9b2c7
Blake Noll <blakenoll@bnollMBP15-MKT.local>
2017-11-06 16:51:08 -0800

    slider and ui updates

--- js/page.js
+++ js/page.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function(){
-  $('.carousel.carousel-slider').carousel({fullWidth: true}, {duration: 300});
-  $('.carousel').carousel({indicators: true});
+
   $('.parallax').parallax();
   $('.scrollspy').scrollSpy();
-  $('.slider').slider();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $('.button-collapse').sideNav();
+
 });
 
 // navbar reveal on scroll controls
@@ -22,10 +23,45 @@ $(function () {
         if ($(this).scrollTop() > 400) {
             $('#menu').fadeIn(550);
         } else {
-            $('#menu').fadeOut(550);
+            $('#menu').fadeOut(300);
         }
     });
 });
 
 });
 }(jQuery));
+
+jQuery(document).ready(function ($) {
+
+  var jssor_1_options = {
+    $AutoPlay: true,
+    $SlideDuration: 800,
+    $SlideEasing: $Jease$.$OutQuint,
+    $ArrowNavigatorOptions: {
+      $Class: $JssorArrowNavigator$
+    },
+    $BulletNavigatorOptions: {
+      $Class: $JssorBulletNavigator$
+    }
+  };
+
+  var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+  /*responsive code begin*/
+  /*you can remove responsive code if you don't want the slider scales while window resizing*/
+  function ScaleSlider() {
+      var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+      if (refSize) {
+          refSize = Math.min(refSize, 1920);
+          jssor_1_slider.$ScaleWidth(refSize);
+      }
+      else {
+          window.setTimeout(ScaleSlider, 30);
+      }
+  }
+  ScaleSlider();
+  $(window).bind("load", ScaleSlider);
+  $(window).bind("resize", ScaleSlider);
+  $(window).bind("orientationchange", ScaleSlider);
+  /*responsive code end*/
+});
